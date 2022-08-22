fbdbff8f060d498702f53863fdcb4c875553849f
Corentin Mercier <corentin@mercier.link>
2022-08-22 14:10:59 +1000

    Mint tokens by unit (instead of smallest decimal)
    mint(100) will mint 100 CAUD instead of 0.00...0100

--- scripts/mint.py
+++ scripts/mint.py
@@ -2,8 +2,12 @@ from ape import project
 from ape.cli import get_user_selected_account
 
 
-def main():
+def mint(token_deployment_id: int, amount: float):
     account = get_user_selected_account()
     token = project.Token
-    contract = token.deployments[0]
-    contract.mint(account, 123, sender=account)
+    contract = token.deployments[token_deployment_id]
+    contract.mint(account, amount * 100000000, sender=account)
+
+
+def main():
+    mint(token_deployment_id=3, amount=100)
