d783cbeed5bc6a6fd27ef22461c162803c4e5bca
Corentin Mercier <corentin@mercier.link>
2022-08-25 21:38:37 +1000

    Run black

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,11 @@ import pytest
 def owner(accounts):
     return accounts[0]
 
+
 @pytest.fixture(scope="session")
 def receiver(accounts):
     return accounts[1]
+
 
 @pytest.fixture(scope="session")
 def token(owner, project):
