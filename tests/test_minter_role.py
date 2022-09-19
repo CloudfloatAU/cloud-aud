0a164abe5df772e161628e62cd343cf0f9acc269
Sitt Guruvanich <aekazitt@gmail.com>
2022-09-19 15:11:16 +0700

    Test revokeMinter scenarios

--- tests/test_minter_role.py
+++ tests/test_minter_role.py
@@ -72,3 +72,40 @@ def test_remove_minter_targeting_zero_address(token, owner, ZERO_ADDRESS):
     with pytest.raises(ape.exceptions.ContractLogicError) as exc_info:
         token.removeMinter(target, sender=owner)
     assert exc_info.value.args[0] == "Targeted address is not a minter"
+
+
+def test_revoke_minter(token, owner, accounts):
+    """
+    Test revoking minter role from address granted minter role by owner
+    """
+    target = accounts[1]
+    token.addMinter(target, sender=owner)
+    assert token.isMinter(target) == True
+    token.revokeMinter(sender=target)
+    assert token.isMinter(target) == False
+
+
+def test_revoke_minter_by_non_minter(token, accounts):
+    """
+    Test revoking minter role from address never granted minter role
+    Must trigger a ContractLogicError (ape.exceptions.ContractLogicError)
+    """
+    target = accounts[1]
+    with pytest.raises(ape.exceptions.ContractLogicError) as exc_info:
+        token.revokeMinter(sender=target)
+    assert exc_info.value.args[0] == "Sender is not a minter"
+
+
+def test_revoke_minter(token, owner, accounts):
+    """
+    Test revoking minter role from address granted minter role by owner, once revoked
+    Must trigger a ContractLogicError (ape.exceptions.ContractLogicError)
+    """
+    target = accounts[1]
+    token.addMinter(target, sender=owner)
+    assert token.isMinter(target) == True
+    token.revokeMinter(sender=target)
+    assert token.isMinter(target) == False
+    with pytest.raises(ape.exceptions.ContractLogicError) as exc_info:
+        token.revokeMinter(sender=target)
+    assert exc_info.value.args[0] == "Sender is not a minter"
