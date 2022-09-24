550a34e3c7f6ec2ce9fcda439e7a73a3d564f0c6
Corentin Mercier <corentin@mercier.link>
2022-09-24 12:15:40 +1000

    Add module docstring

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,3 +1,10 @@
+"""
+Script to deploy the Cloud AUD token (contracts/Token.vy) from a selected wallet to a
+specified network.
+
+See README.md instructions to run this script.
+"""
+
 from ape import project
 from ape.cli import get_user_selected_account
 
