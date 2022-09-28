050cf90c083e79202a85bb3b4e1fa4161826b382
Corentin Mercier <corentin@cloudfloat.com.au>
2022-09-28 17:55:05 +1000

    Test that transferOwnership receiver can mint

--- tests/test_ownership.py
+++ tests/test_ownership.py
@@ -18,5 +18,8 @@ def test_transfer_ownership(token, owner, receiver):
     # test that owner can no longer mint
     with pytest.raises(ape.exceptions.ContractLogicError) as exc_info:
         token.mint(owner, 100, sender=owner)
-
     assert exc_info.value.args[0] == "Access denied."
+
+    # test that the new owner can mint
+    token.mint(receiver, 101, sender=receiver)
+    assert token.balanceOf(receiver) == 101
