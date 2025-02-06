b2e322a6895140f20e3dee4d7d4a390d397884b3
merc1er <corentin@cloudfloat.com.au>
2025-02-06 07:07:09 +0700

    Update docs and migrate to Python 3.13
    Migrate to Shibuya docs theme
    
    Build public API docs; update copyright year
    
    Migrate to Python 3.13

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -7,14 +7,14 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "Cloud AUD"
-copyright = "2022, Cloudfloat"
+copyright = "2025, Cloudfloat"
 author = "Cloudfloat"
 
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", "sphinx_copybutton"]
+extensions = ["myst_parser", "sphinx_copybutton", "sphinxcontrib.openapi"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -23,5 +23,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
+html_theme = "shibuya"
 html_static_path = ["_static"]
